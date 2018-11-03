722aee2d1e1988f73c427d707461ed2a921cf317
john2796 <benedictmiranda27@yahoo.com>
2018-11-03 16:46:23 -0700

    more challenges

--- container/replChallenge.js
+++ container/replChallenge.js
@@ -3,13 +3,160 @@
 // write a function called 'addProperty'
 // Given an object, and a key, 'add Property" sets a new property on the given object with a value of true.
 
-// =======================================================================
-// const myObj = {};
-// function addProperty(obj, key) {
-//   // sets a new property on the given object with a value of true.
-//   return (obj[key] = true);
-// }
+//  1  =======================================================================
+// addProperty Challenge
+/*
+const myObj = {};
+function addProperty(obj, key) {
+  // sets a new property on the given object with a value of true.
+  return (obj[key] = true);
+}
 
-// addProperty(myObj, "myProperty");
-// console.log(myObj.myProperty); // true
+addProperty(myObj, "myProperty");
+console.log(myObj.myProperty); // true
+*/
+//=======================================================================
+
+//  2  =======================================================================
+// getProperty Challenge
+/* 
+Write a function called 'getProperty'.
+Given an object and a key, 'getProperty' returns the value of the property at the given key.
+Notes: If there is no property at the given key, it should return undefiened
+
+const obj = { 
+  key: 'value',
+}
+function getProperty(obj, key) {
+  return obj[key]
+}
+
+const output = getProperty(obj, 'key');
+console.log(output)
+
+*/
+//    =======================================================================
+
+//  3  =======================================================================
+/* removePropert Challenge 
+Write a function called "removeProperty"
+Given an object and a key "removeProperty" removes the given key from the given object
+
+const obj = {
+name: 'Sam',
+age: 20
+}
+
+function removeProperty(obj, key) {
+
+  return delete obj["key"];
+}
+
+removeProperty(obj, 'name');
+console.log(obj.name);
+
+*/
+
+//    =======================================================================
+
+//  4 =======================================================================
+/* checkAge Challenge
+ Instruction 
+ Given person's name and age , "checkAge" returns one of two messages:
+ "Go home, {insert_name_here}!", if they are younger than 21."Welcome, {insert_name_here}!", if they are 21 or older.
+
+Naturally, replace "{insert_name_here}" with the given name. :)
+*/
+/*
+function checkAge(name, age){
+  // your code
+  return age >= 21 ? `Welcome, ${name}!` : `Go home, ${name}!`
+
+}
+
+const output = checkAge('Adrian', 22);
+console.log(output); // --> 'Welcome, Adrian!'
+*/
+
+// =======================================================================
+
+//  5  =======================================================================
+/* Write a function called "getLengthOfWord
+*/
+/*
+function getLengthOfWord(word) {
+  return word.length;
+}
+
+var output = getLengthOfWord('some');
+console.log(output); // --> 4
+*/
+// =======================================================================
+
+//  6  =======================================================================
+/* Write a function called "getLengthOfWord
+*/
+/*
+function getLengthOfWord(word) {
+  return word.length;
+}
+
+var output = getLengthOfWord('some');
+console.log(output); // --> 4
+*/
+// =======================================================================
+
+//  7  =======================================================================
+/* 
+*/
+// =======================================================================
+
+//  8  =======================================================================
+/* 
+*/
+// =======================================================================
+
+//  9  =======================================================================
+/* 
+*/
+// =======================================================================
+
+//  10  =======================================================================
+/* 
+*/
+// =======================================================================
+
+//  11  =======================================================================
+/* 
+*/
+// =======================================================================
+
+//  12  =======================================================================
+/* 
+*/
+// =======================================================================
+
+//  13  =======================================================================
+/* 
+*/
+// =======================================================================
+
+//  14  =======================================================================
+/* 
+*/
+// =======================================================================
+
+//  15  =======================================================================
+/* 
+*/
+// =======================================================================
+
+//  16  =======================================================================
+/* 
+*/
+// =======================================================================
+
+//  17  =======================================================================
+/* 
+*/
 // =======================================================================
