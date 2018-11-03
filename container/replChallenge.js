5a62832b240e5e62ae9c3af80933f48232e2b48e
john2796 <benedictmiranda27@yahoo.com>
2018-11-03 10:59:59 -0700

    added more challenge

--- container/replChallenge.js
+++ container/replChallenge.js
@@ -1,3 +1,15 @@
+/// Working with Object
+
 // write a function called 'addProperty'
-// given an object, and a key. 'addProperty
-console.log("testing");
+// Given an object, and a key, 'add Property" sets a new property on the given object with a value of true.
+
+// =======================================================================
+// const myObj = {};
+// function addProperty(obj, key) {
+//   // sets a new property on the given object with a value of true.
+//   return (obj[key] = true);
+// }
+
+// addProperty(myObj, "myProperty");
+// console.log(myObj.myProperty); // true
+// =======================================================================
