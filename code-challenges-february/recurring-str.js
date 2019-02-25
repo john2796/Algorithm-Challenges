2790d1a38b5e9e6085e1115c1b27ca40cd507088
john2796 <jbmiranda22796@gmail.com>
2019-02-25 06:23:11 -0800

    updated folder and added recurring character challenge [x]

---
+++ code-challenges-february/recurring-str.js
@@ -0,0 +1,26 @@
+/* First Recurring Character 
+
+1. count = { } // dictionary or hash table
+2. loop through str
+3 if char in count 
+4 return char
+5 counts[char] = 1
+6 return null or none if nothing is found
+
+
+*/
+
+let input = "DBCABA";
+function recurringChar(str) {
+  let counts = {};
+
+  for (let char in str) {
+    if (counts.hasOwnProperty(str[char])) {
+      return char;
+    }
+    counts[str[char]] = 1;
+  }
+  console.log(counts);
+  return null;
+}
+recurringChar(input);
