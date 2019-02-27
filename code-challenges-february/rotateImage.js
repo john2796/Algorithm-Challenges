9dbdd427c1b54997061498d5abbec7b1fc732a7a
john2796 <jbmiranda22796@gmail.com>
2019-02-27 09:04:50 -0800

    Added Lambda solution

--- code-challenges-february/rotateImage.js
+++ code-challenges-february/rotateImage.js
@@ -21,3 +21,32 @@ function rotateImage(arr) {
 }
 rotateImage([[1, 2], [3, 4]]);
 // output [ [2, 4],[1, 3]].
+
+/* Added Lambda Solution
+function rotateImage(arr) {
+  const len = arr.length;
+  for (let i = 0; i < len / 2; i++) {
+    
+    for (let j = i; j < len - i - 1; j++) {
+      
+      let bucket = arr[i][j];
+      
+      arr[i][j] = arr[j][len - i - 1];
+      
+      arr[j][len - i - 1] = arr[len - i - 1][len - j - 1];
+      
+      arr[len - i - 1][len - j - 1] = arr[len - j - 1][i];
+      
+      arr[len - j - 1][i] = bucket;
+      
+    }
+    
+  }
+  return arr;
+  
+}
+rotateImage([ [1, 2],
+              [3, 4]]);
+
+
+*/
