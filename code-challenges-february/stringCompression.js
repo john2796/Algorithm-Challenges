8fe0b3cf9a21fd759720428aaaec2a22d7647e87
john2796 <jbmiranda22796@gmail.com>
2019-02-27 14:05:27 -0800

    added stringCompression 3 more version fropm solution cc

--- code-challenges-february/stringCompression.js
+++ code-challenges-february/stringCompression.js
@@ -28,3 +28,68 @@ function stringCompression(str) {
 }
 stringCompression("aabcccccaaa");
 //output = "a2b1c5a3"
+
+// ==================== solution 2 ====================
+
+function stringCompression(str) {
+  let cmpresd = "";
+  const arr = [];
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str.charAt(i) === str.charAt(i + 1)) {
+      count++;
+    } else {
+      arr.push(`${str.charAt(i)}${count}`);
+      count = 1;
+    }
+  }
+  for (let j = 0; j < arr.length; j++) {
+    cmpresd += arr[j];
+  }
+  if (cmpresd.length < str.length) {
+    return cmpresd;
+  } else {
+    return str;
+  }
+}
+// ==================== solution 3 ====================
+
+// // v1.1 .split, .forEach() & .charAt()
+// function stringCompression(str) {
+//   let charArr = str.split('');
+//   let cmpresd = '';
+//   const arr = [];
+//   let count = 1;
+//   charArr.forEach(function(item, i) {
+//     if (item === str.charAt(i + 1)) count++;
+//     else {
+//       arr.push(`${item}${count}`);
+//       count = 1;
+//     }
+//   });
+//   arr.forEach(function(item) {
+//     cmpresd += item;
+//   });
+//   if (cmpresd.length < str.length) return cmpresd;
+//   else return str;
+// }
+
+// ==================== solution 4 ====================
+
+// // v2.0 All in one for loop with Logical OR || and Conditional Ternary operator.
+// function stringCompression(str) {
+//   let curChar = null;
+//   let cmpresd = '';
+//   let count = 1;
+//   for (let i = 0; i <= str.length; i++) {
+//     if (str[i] === curChar) count++;
+//     if (curChar === null) curChar = str[i];
+//     if (str[i] !== curChar || str[i] === undefined) {
+//       cmpresd += curChar;
+//       cmpresd += count;
+//       count = 1;
+//       curChar = str[i];
+//     }
+//   }
+//   return cmpresd.length < str.length ? cmpresd : str;
+// }
